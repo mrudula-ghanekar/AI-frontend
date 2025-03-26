77dd3ef95a0bfc0cddf1dc98a8cf695e93806b2b
mrudula-ghanekar <67146491+mrudula-ghanekar@users.noreply.github.com>
2025-03-26 10:06:46 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './index.css';
+import './App.css';
 import App from './App';
 import LandingPage from './LandingPage';
 import reportWebVitals from './reportWebVitals';
