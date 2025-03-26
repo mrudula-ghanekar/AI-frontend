ac7fab0783d1251ba4fc30e678cd3766121eb64d
mrudula-ghanekar <67146491+mrudula-ghanekar@users.noreply.github.com>
2025-03-26 10:13:40 +0530

    ui

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import LandingPage from './LandingPage';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/analyze" element={<App />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-reportWebVitals();
