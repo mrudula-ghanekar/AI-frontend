48e1452c03141960b910cd9a7a3a74641e93a2a8
mrudula-ghanekar <67146491+mrudula-ghanekar@users.noreply.github.com>
2025-09-23 23:55:40 +0530

    ok

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
+import Landing from './components/LandingPage';
 import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<Landing />} />
+      <Route path="/app" element={<App />} />
     </Routes>
   </Router>
 );
